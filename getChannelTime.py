97b0526f8bf4ac848e9f361700ab62cc211f41f4
Steven Jacobs <sjaco002@ucr.edu>
2018-04-11 12:48:31 -0700

    Added ChannelTime script

---
+++ getChannelTime.py
@@ -0,0 +1,15 @@
+log=open("../asterixdb/logs/cc.log")
+i=0
+times = [0,0,0,0,0,0,0,0,0,0]
+for line in log:
+	if (len(line.split(" ")) < 14):
+		continue
+	if (line.split(" ")[13] == "Took"):
+		i=i+1
+		times[i%10] = int(line.split(" ")[14])
+
+print times
+total = 0
+for item in times:
+	total = total + item
+print str(total/10)
