e501ef61fd65e4f671de2d2726306db8db1e8c26
Steven Jacobs <sjaco002@ucr.edu>
2018-04-24 12:55:29 -0700

    Updates

--- getChannelTime.py
+++ getChannelTime.py
@@ -1,15 +1,16 @@
 log=open("../asterixdb/logs/cc.log")
 i=0
-times = [0,0,0,0,0,0,0,0,0,0]
+times = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 for line in log:
 	if (len(line.split(" ")) < 14):
 		continue
 	if (line.split(" ")[13] == "Took"):
+		print line
 		i=i+1
-		times[i%10] = int(line.split(" ")[14])
+		times[i%20] = int(line.split(" ")[14])
 
 print times
 total = 0
 for item in times:
 	total = total + item
-print str(total/10)
+print str(total/20)
